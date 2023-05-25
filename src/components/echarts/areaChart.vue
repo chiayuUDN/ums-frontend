ff5bf043b588cb7fc116752c017d5c38811218ad
張珈語 <chiayu.chang@udngroup.com.tw>
2023-05-25 15:36:31 +0800

    實作 area chart

---
+++ src/components/echarts/areaChart.vue
@@ -0,0 +1,54 @@
+<!-- 面積圖 -->
+<template>
+    <chart :option="option"></chart>
+</template>
+<script>
+import chart from './chart.vue';
+
+export default {
+    props: {
+        data: Object,
+    },
+    components: { chart },
+    computed: {
+        names() {
+            return Object.keys(this.data);
+        },
+        values() {
+            return Object.values(this.data);
+        },
+        option() {
+            return {
+                title: {
+                    text: '柱狀圖示例',
+                    subtext: '銷售數據',
+                    left: 'center',
+                },
+                tooltip: {
+
+                },
+                legend: {
+                    orient: 'vertical',
+                    left: 'left',
+                },
+                xAxis: {
+                    type: 'category',
+                    data: this.names
+                },
+                yAxis: {
+                    type: 'value'
+                },
+                series: [
+                    {
+                        type: 'line',
+                        areaStyle: {},
+                        stack: 'Total',
+                        name: 'test1',
+                        data: this.values
+                    }
+                ]
+            }
+        }
+    }
+}
+</script>
